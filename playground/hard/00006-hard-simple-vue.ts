05047f8b3eb7b3d46bdda1bb13310001376d2156
Martin Elsaesser <marels@aon.at>
2025-09-21 13:57:43 +0200

    implement 00006

--- playground/hard/00006-hard-simple-vue.ts
+++ playground/hard/00006-hard-simple-vue.ts
@@ -46,46 +46,61 @@
 
 /* _____________ Your Code Here _____________ */
 
-declare function SimpleVue(options: any): any
+type ComputedThis<TComputed extends Computed> = {
+	[P in keyof TComputed]: ReturnType<TComputed[P]>;
+};
+
+type Methods = Record<string, (...args: any[]) => any>;
+type Computed = Record<string, () => any>;
+
+type Options<TData, TComputed extends Computed, TMethods extends Methods> = {
+	data: (this: void) => TData;
+	computed: TComputed & ThisType<TData>;
+	methods: TMethods & ThisType<TMethods & TData & ComputedThis<TComputed>>;
+};
+
+declare function SimpleVue<TData, TMethods extends Methods, TComputed extends Computed>(
+	options: Options<TData, TComputed, TMethods>
+): any;
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 SimpleVue({
-  data() {
-    // @ts-expect-error
-    this.firstname
-    // @ts-expect-error
-    this.getRandom()
-    // @ts-expect-error
-    this.data()
+	data() {
+		// @ts-expect-error
+		this.firstname;
+		// @ts-expect-error
+		this.getRandom();
+		// @ts-expect-error
+		this.data();
 
-    return {
-      firstname: 'Type',
-      lastname: 'Challenges',
-      amount: 10,
-    }
-  },
-  computed: {
-    fullname() {
-      return `${this.firstname} ${this.lastname}`
-    },
-  },
-  methods: {
-    getRandom() {
-      return Math.random()
-    },
-    hi() {
-      alert(this.amount)
-      alert(this.fullname.toLowerCase())
-      alert(this.getRandom())
-    },
-    test() {
-      const fullname = this.fullname
-      const cases: [Expect<Equal<typeof fullname, string>>] = [] as any
-    },
-  },
-})
+		return {
+			firstname: "Type",
+			lastname: "Challenges",
+			amount: 10,
+		};
+	},
+	computed: {
+		fullname() {
+			return `${this.firstname} ${this.lastname}`;
+		},
+	},
+	methods: {
+		getRandom() {
+			return Math.random();
+		},
+		hi() {
+			alert(this.amount);
+			alert(this.fullname.toLowerCase());
+			alert(this.getRandom());
+		},
+		test() {
+			const fullname = this.fullname;
+			const cases: [Expect<Equal<typeof fullname, string>>] = [] as any;
+		},
+	},
+});
 
 /* _____________ Further Steps _____________ */
 /*
