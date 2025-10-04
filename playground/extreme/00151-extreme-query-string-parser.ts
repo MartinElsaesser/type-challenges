298e843e41c81cb9dd5a51bc404e53f826f88bc4
Martin Elsaesser <marels@aon.at>
2025-10-04 16:51:40 +0200

    refactor 00151

--- playground/extreme/00151-extreme-query-string-parser.ts
+++ playground/extreme/00151-extreme-query-string-parser.ts
@@ -21,16 +21,16 @@
 
 import { expectTypeOf } from "expect-type";
 
-type JoinIntersections<T> = {
-	[K in keyof T]: T[K];
-};
-
 type TokenizeQueryParams<TParam extends string> =
-	TParam extends "" ? {}
-	: TParam extends `${infer Key}=${infer Value}` ? { [K in Key]: Value }
-	: { [K in TParam]: true };
-type QueryParamToken = TokenizeQueryParams<any>;
+	TParam extends "" ? never
+	: TParam extends `${infer Key}=${infer Value}` ? { key: Key; value: Value }
+	: { key: TParam; value: true };
 
+type TokenValues = string | true;
+type QueryParamToken = {
+	key: string;
+	value: TokenValues;
+};
 type Tokenizer<S extends string> =
 	S extends `${infer W1}&${infer W2}` ? [TokenizeQueryParams<W1>, ...Tokenizer<W2>]
 	:	[TokenizeQueryParams<S>];
@@ -40,62 +40,75 @@ type Parser<
 	Collect extends Record<string, TokenValues[] | TokenValues> = {},
 > =
 	Tokens extends [infer F extends QueryParamToken, ...infer R extends QueryParamToken[]] ?
-		Parser<R, MergeObjIntoArr<F, Collect>>
+		Parser<R, MergeTokenIntoParams<F, Collect>>
 	:	Collect;
 
-type ParseQueryString<S extends string> = JoinIntersections<Parser<Tokenizer<S>>>;
+type ParseQueryString<S extends string> = Parser<Tokenizer<S>>;
 
-type TokenValues = string | true;
-type MergeObjIntoArr<
-	TObj extends Record<string, TokenValues>,
-	TArr extends Record<string, TokenValues[] | TokenValues>,
-	CommonKeys extends string = Extract<keyof TObj & string, keyof TArr & string>,
-> = Omit<TObj, CommonKeys> &
-	Omit<TArr, CommonKeys> & {
-		[P in CommonKeys]: TArr[P] extends TokenValues[] ? StringArrMerge<TObj[P], TArr[P]>
-		:	StringMerge<TObj[P], TArr[P] & TokenValues>;
-	};
+type MergeTokenIntoParams<
+	Token extends QueryParamToken,
+	Params extends Record<string, TokenValues[] | TokenValues>,
+> =
+	[Token] extends [never] ? Params
+	:	{
+			[P in keyof Params | Token["key"]]: P extends keyof Params ?
+				P extends Token["key"] ?
+					// common key
+					MergeValueIntoValues<Token["value"], Params[P]>
+				:	// only keyof TArr
+					Params[P]
+			: P extends Token["key"] ?
+				// only Token key
+				Token["value"]
+			:	never;
+		};
 
-type StringArrMerge<T extends TokenValues, TArr extends TokenValues[]> =
-	T extends TArr[number] ? TArr : [...TArr, T];
-type StringMerge<T2 extends TokenValues, T1 extends TokenValues> =
-	[T2, T1] extends [T1, T2] ? T1 : [T1, T2];
+type MergeValueIntoValues<Value extends TokenValues, Values extends TokenValues[] | TokenValues> =
+	Values extends TokenValues[] ?
+		// Values is an array
+		Value extends Values[number] ?
+			Values
+		:	[...Values, Value]
+	: // Values is not an array
+	[Value, Values] extends [Values, Value] ? Values
+	: [Values, Value];
 
-type TMO<
-	TObj extends Record<string, TokenValues>,
+type $MVIV<
+	Token extends QueryParamToken,
 	TArr extends Record<string, TokenValues | TokenValues[]>,
-> = JoinIntersections<MergeObjIntoArr<TObj, TArr>>;
+> = MergeTokenIntoParams<Token, TArr>;
 
 // independent objects
-expectTypeOf<{
-	k1: "v1";
-	k2: "v2";
-	k3: "v3";
-	k4: "v4";
-}>().toEqualTypeOf<TMO<{ k2: "v2"; k4: "v4" }, { k1: "v1"; k3: "v3" }>>();
-expectTypeOf<{ k1: "v1"; k2: "v2" }>().toEqualTypeOf<TMO<{ k2: "v2" }, { k1: "v1" }>>();
-expectTypeOf<{ k1: "v1" }>().toEqualTypeOf<TMO<{ k1: "v1" }, {}>>();
-expectTypeOf<{ k2: "v2" }>().toEqualTypeOf<TMO<{}, { k2: "v2" }>>();
+expectTypeOf<{ k1: "v1"; k2: "v2"; k3: "v3" }>().toEqualTypeOf<
+	MergeTokenIntoParams<{ key: "k2"; value: "v2" }, { k1: "v1"; k3: "v3" }>
+>();
+expectTypeOf<{ k2: "v2" }>().toEqualTypeOf<MergeTokenIntoParams<{ key: "k2"; value: "v2" }, {}>>();
 
 // dependent objects: no duplicates
-expectTypeOf<{ k1: ["v1", "v2"] }>().toEqualTypeOf<TMO<{ k1: "v2" }, { k1: "v1" }>>();
-expectTypeOf<{ k1: ["v0", "v1"] }>().toEqualTypeOf<TMO<{ k1: "v1" }, { k1: "v0" }>>();
+expectTypeOf<{ k1: ["v1", "v2"] }>().toEqualTypeOf<
+	$MVIV<{ key: "k1"; value: "v2" }, { k1: "v1" }>
+>();
+expectTypeOf<{ k1: ["v0", "v1"] }>().toEqualTypeOf<
+	$MVIV<{ key: "k1"; value: "v1" }, { k1: "v0" }>
+>();
 
 expectTypeOf<{
 	k1: ["v1", "v2", "v3"];
-}>().toEqualTypeOf<TMO<{ k1: "v3" }, { k1: ["v1", "v2"] }>>();
+}>().toEqualTypeOf<$MVIV<{ key: "k1"; value: "v3" }, { k1: ["v1", "v2"] }>>();
 
 expectTypeOf<{
 	k1: ["v1", "v2"];
-}>().toEqualTypeOf<TMO<{}, { k1: ["v1", "v2"] }>>();
+}>().toEqualTypeOf<$MVIV<never, { k1: ["v1", "v2"] }>>();
 
 // dependent objects: duplicates
-expectTypeOf<{ k1: true }>().toEqualTypeOf<TMO<{ k1: true }, { k1: true }>>();
-expectTypeOf<{ k1: [true, "false"] }>().toEqualTypeOf<TMO<{ k1: true }, { k1: [true, "false"] }>>();
-expectTypeOf<{ k1: "v1" }>().toEqualTypeOf<TMO<{ k1: "v1" }, { k1: "v1" }>>();
+expectTypeOf<{ k1: true }>().toEqualTypeOf<$MVIV<{ key: "k1"; value: true }, { k1: true }>>();
+expectTypeOf<{ k1: [true, "false"] }>().toEqualTypeOf<
+	$MVIV<{ key: "k1"; value: true }, { k1: [true, "false"] }>
+>();
+expectTypeOf<{ k1: "v1" }>().toEqualTypeOf<$MVIV<{ key: "k1"; value: "v1" }, { k1: "v1" }>>();
 expectTypeOf<{
 	k1: ["v1", "v2"];
-}>().toEqualTypeOf<TMO<{ k1: "v1" }, { k1: ["v1", "v2"] }>>();
+}>().toEqualTypeOf<$MVIV<{ key: "k1"; value: "v1" }, { k1: ["v1", "v2"] }>>();
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from "@type-challenges/utils";
