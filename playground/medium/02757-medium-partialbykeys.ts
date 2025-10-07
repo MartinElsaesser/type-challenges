c2630ef101e66bf2529b5b9b245471f729ce0cde
Martin Elsaesser <marels@aon.at>
2025-10-07 20:50:25 +0200

    solve 02757

--- playground/medium/02757-medium-partialbykeys.ts
+++ playground/medium/02757-medium-partialbykeys.ts
@@ -26,36 +26,40 @@
 
 /* _____________ Your Code Here _____________ */
 
-type PartialByKeys<T, K> = any
+type PartialByKeys<T, K extends keyof T = keyof T> = Prettify<Partial<Pick<T, K>> & Omit<T, K>>;
+
+type Prettify<X1> = {
+	[P in keyof X1]: X1[P];
+};
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 interface User {
-  name: string
-  age: number
-  address: string
+	name: string;
+	age: number;
+	address: string;
 }
 
 interface UserPartialName {
-  name?: string
-  age: number
-  address: string
+	name?: string;
+	age: number;
+	address: string;
 }
 
 interface UserPartialNameAndAge {
-  name?: string
-  age?: number
-  address: string
+	name?: string;
+	age?: number;
+	address: string;
 }
 
 type cases = [
-  Expect<Equal<PartialByKeys<User, 'name'>, UserPartialName>>,
-  Expect<Equal<PartialByKeys<User, 'name' | 'age'>, UserPartialNameAndAge>>,
-  Expect<Equal<PartialByKeys<User>, Partial<User>>>,
-  // @ts-expect-error
-  Expect<Equal<PartialByKeys<User, 'name' | 'unknown'>, UserPartialName>>,
-]
+	Expect<Equal<PartialByKeys<User, "name">, UserPartialName>>,
+	Expect<Equal<PartialByKeys<User, "name" | "age">, UserPartialNameAndAge>>,
+	Expect<Equal<PartialByKeys<User>, Partial<User>>>,
+	// @ts-expect-error
+	Expect<Equal<PartialByKeys<User, "name" | "unknown">, UserPartialName>>,
+];
 
 /* _____________ Further Steps _____________ */
 /*
