33daf6ca8fe20f63f24595466b044624902b7f4a
Martin Elsaesser <marels@aon.at>
2025-10-10 14:22:18 +0200

    solve 00949

--- playground/medium/00949-medium-anyof.ts
+++ playground/medium/00949-medium-anyof.ts
@@ -19,23 +19,25 @@
 
 /* _____________ Your Code Here _____________ */
 
-type AnyOf<T extends readonly any[]> = any
+// type IsTruthy<T> = T extends `${infer _1}${infer _2}` ? true : false;
+type Falsey = 0 | "" | false | [] | Record<PropertyKey, never> | null | undefined;
+type AnyOf<T extends readonly any[]> = T[number] extends Falsey ? false : true;
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 type cases = [
-  Expect<Equal<AnyOf<[1, 'test', true, [1], { name: 'test' }, { 1: 'test' }]>, true>>,
-  Expect<Equal<AnyOf<[1, '', false, [], {}]>, true>>,
-  Expect<Equal<AnyOf<[0, 'test', false, [], {}]>, true>>,
-  Expect<Equal<AnyOf<[0, '', true, [], {}]>, true>>,
-  Expect<Equal<AnyOf<[0, '', false, [1], {}]>, true>>,
-  Expect<Equal<AnyOf<[0, '', false, [], { name: 'test' }]>, true>>,
-  Expect<Equal<AnyOf<[0, '', false, [], { 1: 'test' }]>, true>>,
-  Expect<Equal<AnyOf<[0, '', false, [], { name: 'test' }, { 1: 'test' }]>, true>>,
-  Expect<Equal<AnyOf<[0, '', false, [], {}, undefined, null]>, false>>,
-  Expect<Equal<AnyOf<[]>, false>>,
-]
+	Expect<Equal<AnyOf<[1, "test", true, [1], { name: "test" }, { 1: "test" }]>, true>>,
+	Expect<Equal<AnyOf<[1, "", false, [], {}]>, true>>,
+	Expect<Equal<AnyOf<[0, "test", false, [], {}]>, true>>,
+	Expect<Equal<AnyOf<[0, "", true, [], {}]>, true>>,
+	Expect<Equal<AnyOf<[0, "", false, [1], {}]>, true>>,
+	Expect<Equal<AnyOf<[0, "", false, [], { name: "test" }]>, true>>,
+	Expect<Equal<AnyOf<[0, "", false, [], { 1: "test" }]>, true>>,
+	Expect<Equal<AnyOf<[0, "", false, [], { name: "test" }, { 1: "test" }]>, true>>,
+	Expect<Equal<AnyOf<[0, "", false, [], {}, undefined, null]>, false>>,
+	Expect<Equal<AnyOf<[]>, false>>,
+];
 
 /* _____________ Further Steps _____________ */
 /*
