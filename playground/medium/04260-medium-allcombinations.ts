fe13cd2faf5deaf9a8a7379eab5c1f480feba46a
Martin Elsaesser <marels@aon.at>
2025-10-14 21:59:47 +0200

    solve 04260

--- playground/medium/04260-medium-allcombinations.ts
+++ playground/medium/04260-medium-allcombinations.ts
@@ -19,18 +19,115 @@
 
 /* _____________ Your Code Here _____________ */
 
-type AllCombinations<S> = any
+type AllCombinationsHelper<T extends string, _T2 extends string = T> =
+	[T] extends [never] ? ""
+	: _T2 extends _T2 ? `${_T2 | ""}${AllCombinationsHelper<Exclude<T, _T2>>}`
+	: "";
+
+type UnionFromString<S extends string> =
+	S extends `${infer F}${infer R}` ? F | UnionFromString<R> : "";
+
+type AllCombinations<S extends string> = AllCombinationsHelper<UnionFromString<S>>;
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 type cases = [
-  Expect<Equal<AllCombinations<''>, ''>>,
-  Expect<Equal<AllCombinations<'A'>, '' | 'A'>>,
-  Expect<Equal<AllCombinations<'AB'>, '' | 'A' | 'B' | 'AB' | 'BA'>>,
-  Expect<Equal<AllCombinations<'ABC'>, '' | 'A' | 'B' | 'C' | 'AB' | 'AC' | 'BA' | 'BC' | 'CA' | 'CB' | 'ABC' | 'ACB' | 'BAC' | 'BCA' | 'CAB' | 'CBA'>>,
-  Expect<Equal<AllCombinations<'ABCD'>, '' | 'A' | 'B' | 'C' | 'D' | 'AB' | 'AC' | 'AD' | 'BA' | 'BC' | 'BD' | 'CA' | 'CB' | 'CD' | 'DA' | 'DB' | 'DC' | 'ABC' | 'ABD' | 'ACB' | 'ACD' | 'ADB' | 'ADC' | 'BAC' | 'BAD' | 'BCA' | 'BCD' | 'BDA' | 'BDC' | 'CAB' | 'CAD' | 'CBA' | 'CBD' | 'CDA' | 'CDB' | 'DAB' | 'DAC' | 'DBA' | 'DBC' | 'DCA' | 'DCB' | 'ABCD' | 'ABDC' | 'ACBD' | 'ACDB' | 'ADBC' | 'ADCB' | 'BACD' | 'BADC' | 'BCAD' | 'BCDA' | 'BDAC' | 'BDCA' | 'CABD' | 'CADB' | 'CBAD' | 'CBDA' | 'CDAB' | 'CDBA' | 'DABC' | 'DACB' | 'DBAC' | 'DBCA' | 'DCAB' | 'DCBA'>>,
-]
+	Expect<Equal<AllCombinations<"">, "">>,
+	Expect<Equal<AllCombinations<"A">, "" | "A">>,
+	Expect<Equal<AllCombinations<"AB">, "" | "A" | "B" | "AB" | "BA">>,
+	Expect<
+		Equal<
+			AllCombinations<"ABC">,
+			| ""
+			| "A"
+			| "B"
+			| "C"
+			| "AB"
+			| "AC"
+			| "BA"
+			| "BC"
+			| "CA"
+			| "CB"
+			| "ABC"
+			| "ACB"
+			| "BAC"
+			| "BCA"
+			| "CAB"
+			| "CBA"
+		>
+	>,
+	Expect<
+		Equal<
+			AllCombinations<"ABCD">,
+			| ""
+			| "A"
+			| "B"
+			| "C"
+			| "D"
+			| "AB"
+			| "AC"
+			| "AD"
+			| "BA"
+			| "BC"
+			| "BD"
+			| "CA"
+			| "CB"
+			| "CD"
+			| "DA"
+			| "DB"
+			| "DC"
+			| "ABC"
+			| "ABD"
+			| "ACB"
+			| "ACD"
+			| "ADB"
+			| "ADC"
+			| "BAC"
+			| "BAD"
+			| "BCA"
+			| "BCD"
+			| "BDA"
+			| "BDC"
+			| "CAB"
+			| "CAD"
+			| "CBA"
+			| "CBD"
+			| "CDA"
+			| "CDB"
+			| "DAB"
+			| "DAC"
+			| "DBA"
+			| "DBC"
+			| "DCA"
+			| "DCB"
+			| "ABCD"
+			| "ABDC"
+			| "ACBD"
+			| "ACDB"
+			| "ADBC"
+			| "ADCB"
+			| "BACD"
+			| "BADC"
+			| "BCAD"
+			| "BCDA"
+			| "BDAC"
+			| "BDCA"
+			| "CABD"
+			| "CADB"
+			| "CBAD"
+			| "CBDA"
+			| "CDAB"
+			| "CDBA"
+			| "DABC"
+			| "DACB"
+			| "DBAC"
+			| "DBCA"
+			| "DCAB"
+			| "DCBA"
+		>
+	>,
+];
 
 /* _____________ Further Steps _____________ */
 /*
