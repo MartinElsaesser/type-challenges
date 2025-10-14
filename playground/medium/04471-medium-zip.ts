2afb78b87d76d6f7e24f67ec9953464cbe4e71c8
Martin Elsaesser <marels@aon.at>
2025-10-14 22:20:36 +0200

    solve 04471

--- playground/medium/04471-medium-zip.ts
+++ playground/medium/04471-medium-zip.ts
@@ -15,18 +15,26 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Zip<T, U> = any
+type Zip<T, U, R extends any[] = []> =
+	T extends [infer T_First, ...infer T_Rest] ?
+		U extends [infer U_First, ...infer U_Rest] ?
+			Zip<T_Rest, U_Rest, [...R, [T_First, U_First]]>
+		:	R
+	:	R;
+
+type debug = Zip<[1, 2], [true, false]>;
+//   ^?
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 type cases = [
-  Expect<Equal<Zip<[], []>, []>>,
-  Expect<Equal<Zip<[1, 2], [true, false]>, [[1, true], [2, false]]>>,
-  Expect<Equal<Zip<[1, 2, 3], ['1', '2']>, [[1, '1'], [2, '2']]>>,
-  Expect<Equal<Zip<[], [1, 2, 3]>, []>>,
-  Expect<Equal<Zip<[[1, 2]], [3]>, [[[1, 2], 3]]>>,
-]
+	Expect<Equal<Zip<[], []>, []>>,
+	Expect<Equal<Zip<[1, 2], [true, false]>, [[1, true], [2, false]]>>,
+	Expect<Equal<Zip<[1, 2, 3], ["1", "2"]>, [[1, "1"], [2, "2"]]>>,
+	Expect<Equal<Zip<[], [1, 2, 3]>, []>>,
+	Expect<Equal<Zip<[[1, 2]], [3]>, [[[1, 2], 3]]>>,
+];
 
 /* _____________ Further Steps _____________ */
 /*
